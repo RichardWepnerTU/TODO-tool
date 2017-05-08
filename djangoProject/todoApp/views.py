2ac1d85f6c38b7e5aaa7d4844d5866d6e8b9149d
Richard Wepner <wepner@campus.tu-berlin.de>
2017-05-08 11:38:57 +0200

    integrated bootstrap, basic implementation of the todo list
    integrated bootstrap
    basic implementation of the todo list

--- djangoProject/todoApp/views.py
+++ djangoProject/todoApp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import ToDoEntry
 
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
-	
+	return todoList(request)
+
+def todoList(request):
+    todoEntries = ToDoEntry.objects.order_by('dueDate')
+    template = loader.get_template('todoList.html')
+    context = {
+        'todoEntries': todoEntries,
+    }
+    return HttpResponse(template.render(context, request))
+    
+def todoEntry(request, id):
+    return HTTPResponse("" % id)
