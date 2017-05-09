c2e3932037eaf58a7259044d38e368eac78b4733
s.kotte <s.kotte@web.de>
2017-05-09 17:08:10 +0200

    addes todoput and todoput

--- djangoProject/todoApp/urls.py
+++ djangoProject/todoApp/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^todo/', views.index, name='index')
+    url(r'^todo/', views.index, name='index'),
+    url(r'^list/', views.requestMethod, name='list')
 ]
--- djangoProject/todoApp/views.py
+++ djangoProject/todoApp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.template import loader
+from django.forms.models import modelform_factory
+from django.core import serializers
 
 from .models import ToDoEntry
 
@@ -15,6 +17,42 @@ def todoList(request):
         'todoEntries': todoEntries,
     }
     return HttpResponse(template.render(context, request))
-    
-def todoEntry(request, id):
-    return HTTPResponse("" % id)
+
+def requestMethod(request):
+	if request.method == 'GET':
+	    todoGet(request)
+	elif request.method == 'PUT':
+	    todoPut(request, id)
+	elif request.method == 'DELETE':
+	    todoDelete(request, id)
+
+#get spectific entry    
+#def todoEntry(request, id):
+    #return HTTPResponse("" % id)
+
+#returns all current object in db
+def todoGet(request):
+	data = []
+	for elem in ToDoEntry.objects.all():
+		data.append({'id':elem.id, 'name':elem.name, 'description':elem.description, 'dueDate':elem.dueDate, 'progress':elem.progress})
+	return JsonResponse(data, safe=False)
+
+#put
+def todoPut(request):
+	modelform = modelform_factory(ToDoEntry)
+	form = modelform(request.PUT)
+	if form.is_valid():
+		form.save()
+	return redirect('restview')
+
+#post
+def todoPost(request, id):
+	pass
+	#obj = ToDoEntry.objects.get(pk=id)
+
+
+#delete
+def todoDelete(request, id):
+	pass
+	#ToDoEntry.objects.get(id).delete()
+	#return HTTPResponse()
